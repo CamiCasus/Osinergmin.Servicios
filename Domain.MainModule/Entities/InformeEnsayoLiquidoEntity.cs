661dcef71222b187d335e08621981b7cd83892f6
Carlos Francia Segura <cfrancia@sigcomt.com>
2018-02-09 15:49:56 -0500

    Clase Servicios

--- Domain.MainModule/Entities/InformeEnsayoLiquidoEntity.cs
+++ Domain.MainModule/Entities/InformeEnsayoLiquidoEntity.cs
@@ -6,5 +6,65 @@ namespace Domain.MainModule.Entities
 {
     public class InformeEnsayoLiquidoEntity : InformeEnsayoEntity
     {
+        public string observaciones { get; set; }
+        public string numeroInformeLaboratorio { get; set; }
+        public string octanaje { get; set; }
+        public string puntoInflamacion { get; set; }
+        public int presionVaporReid { get; set; }
+        public string contenidoAzufre4294 { get; set; }
+        public string contenidoAzufre5453 { get; set; }
+        public int viscosidadCinematica { get; set; }
+        public int contenidoManganeso { get; set; }
+        public string puntoEscurrimiento { get; set; }
+        public string contenidoPlomo { get; set; }
+        public string contenidoPlomoAstmd3341 { get; set; }
+        public string contenidoPlomoAstmd3237 { get; set; }
+        public int astmd86_pie { get; set; }
+        public int astmd86_5p { get; set; }
+        public int astmd86_10p { get; set; }
+        public int astmd86_20p { get; set; }
+        public int astmd86_50p { get; set; }
+        public int astmd86_90p { get; set; }
+        public int astmd86_95p { get; set; }
+        public int astmd86_pfe { get; set; }
+        public int astmd86_recup { get; set; }
+        public int aastmd86_residuo { get; set; }
+        public int aastmd86_perdidas { get; set; }
+        public int gravidadApi { get; set; }
+        public int densidadRelativa { get; set; }
+        public int indiceCetano { get; set; }
+        public int indiceCetanoProcedenciaA { get; set; }
+        public int indiceCetanoProcedenciaB { get; set; }
+        public int indiceCetanoBajoAzufre { get; set; }
+        public int contenidoFameVolumen { get; set; }
+        public string contenidoEtanolVolumen { get; set; }
+        public string totalOxigenadosVolumen { get; set; }
+        public string totalOxigeno { get; set; }
+        public string contenidoMtbeVolumen { get; set; }
+        public string contenidoTameVolumen { get; set; }
+        public string contenidoEtbeVolumen { get; set; }
+        public string contenidoMetanolVolumen { get; set; }
+        public string contenidoTertbutanoVolumen { get; set; }
+        public string contenidoDipeVolumen { get; set; }
+        public string contenidoMtbeMasa { get; set; }
+        public string contenidoTameMasa { get; set; }
+        public string contenidoEtbeMasa { get; set; }
+        public string contenidoEtanolMasa { get; set; }
+        public string contenidoMetanolMasa { get; set; }
+        public string contenidoTertbutanoMasa { get; set; }
+        public string contenidoDipeMasa { get; set; }
+        public string totalOxigenadosMasa { get; set; }
+        public int reaccionAlAgua { get; set; }
+        public string contenidoSolidos { get; set; }
+        public string contenidoGomas { get; set; }
+        public string puntoCongelamiento { get; set; }
+        public string aguaSedimentos { get; set; }
+        public int determinacionBenceno { get; set; }
+        public string aguaPorDestilacion { get; set; }
+        public string contaminacionParticulas { get; set; }
+        public string indiceCetanoFme { get; set; }
+        public string color { get; set; }
+        public string resultadoFinal { get; set; }
+        public string remanenteRetirado { get; set; }
     }
 }
--- Infraestructura.Data.MainModule/OsinergminRepository.cs
+++ Infraestructura.Data.MainModule/OsinergminRepository.cs
@@ -156,8 +156,66 @@ namespace Infraestructura.Data.MainModule
             var responseClienteOsinergmin = await _osinergminClient.registrarInformeLabLiquidoCalidadAsync(
                 new informeLiquido
                 {
-
-                });
+        observaciones = informeEnsayoLiquido.observaciones,
+        numeroInformeLaboratorio = informeEnsayoLiquido.numeroInformeLaboratorio,
+        octanaje = informeEnsayoLiquido.octanaje,
+        puntoInflamacion = informeEnsayoLiquido.puntoInflamacion,
+        presionVaporReid = informeEnsayoLiquido.presionVaporReid,
+        contenidoAzufre4294 = informeEnsayoLiquido.contenidoAzufre4294,
+        contenidoAzufre5453 = informeEnsayoLiquido.contenidoAzufre5453,
+        viscosidadCinematica = informeEnsayoLiquido.viscosidadCinematica,
+        contenidoManganeso = informeEnsayoLiquido.contenidoManganeso,
+        puntoEscurrimiento = informeEnsayoLiquido.puntoEscurrimiento,
+        contenidoPlomo = informeEnsayoLiquido.contenidoPlomo,
+        contenidoPlomoAstmd3341 = informeEnsayoLiquido.contenidoPlomoAstmd3341,
+        contenidoPlomoAstmd3237 = informeEnsayoLiquido.contenidoPlomoAstmd3237,
+        astmd86_pie = informeEnsayoLiquido.astmd86_pie,
+        astmd86_5p = informeEnsayoLiquido.astmd86_5p,
+        astmd86_10p = informeEnsayoLiquido.astmd86_10p,
+        astmd86_20p = informeEnsayoLiquido.astmd86_20p,
+        astmd86_50p = informeEnsayoLiquido.astmd86_50p,
+        astmd86_90p = informeEnsayoLiquido.astmd86_90p,
+        astmd86_95p = informeEnsayoLiquido.astmd86_95p,
+        astmd86_pfe = informeEnsayoLiquido.astmd86_pfe,
+        astmd86_recup = informeEnsayoLiquido.astmd86_recup,
+        astmd86_residuo = informeEnsayoLiquido.aastmd86_residuo,
+        astmd86_perdidas = informeEnsayoLiquido.aastmd86_perdidas,
+        gravidadApi = informeEnsayoLiquido.gravidadApi,
+        densidadRelativa = informeEnsayoLiquido.densidadRelativa,
+        indiceCetano = informeEnsayoLiquido.indiceCetano,
+        indiceCetanoProcedenciaA = informeEnsayoLiquido.indiceCetanoProcedenciaA,
+        indiceCetanoProcedenciaB = informeEnsayoLiquido.indiceCetanoProcedenciaB,
+        indiceCetanoBajoAzufre = informeEnsayoLiquido.indiceCetanoBajoAzufre,
+        contenidoFameVolumen = informeEnsayoLiquido.contenidoFameVolumen,
+        contenidoEtanolVolumen = informeEnsayoLiquido.contenidoEtanolVolumen,
+        totalOxigenadosVolumen = informeEnsayoLiquido.totalOxigenadosVolumen,
+        totalOxigeno = informeEnsayoLiquido.totalOxigeno,
+        contenidoMtbeVolumen = informeEnsayoLiquido.contenidoMtbeVolumen,
+        contenidoTameVolumen = informeEnsayoLiquido.contenidoTameVolumen,
+        contenidoEtbeVolumen = informeEnsayoLiquido.contenidoEtbeVolumen,
+        contenidoMetanolVolumen = informeEnsayoLiquido.contenidoMetanolVolumen,
+        contenidoTertbutanoVolumen = informeEnsayoLiquido.contenidoTertbutanoVolumen,
+        contenidoDipeVolumen = informeEnsayoLiquido.contenidoDipeVolumen,
+        contenidoMtbeMasa = informeEnsayoLiquido.contenidoMtbeMasa,
+        contenidoTameMasa = informeEnsayoLiquido.contenidoTameMasa,
+        contenidoEtbeMasa = informeEnsayoLiquido.contenidoEtbeMasa,
+        contenidoEtanolMasa = informeEnsayoLiquido.contenidoEtanolMasa,
+        contenidoMetanolMasa = informeEnsayoLiquido.contenidoMetanolMasa,
+        contenidoTertbutanoMasa = informeEnsayoLiquido.contenidoTertbutanoMasa,
+        contenidoDipeMasa = informeEnsayoLiquido.contenidoDipeMasa,
+        totalOxigenadosMasa = informeEnsayoLiquido.totalOxigenadosMasa,
+        reaccionAlAgua = informeEnsayoLiquido.reaccionAlAgua,
+        contenidoSolidos = informeEnsayoLiquido.contenidoSolidos,
+        contenidoGomas = informeEnsayoLiquido.contenidoGomas,
+        puntoCongelamiento = informeEnsayoLiquido.puntoCongelamiento,
+        determinacionBenceno = informeEnsayoLiquido.determinacionBenceno,
+        aguaPorDestilacion = informeEnsayoLiquido.aguaPorDestilacion,
+        contaminacionParticulas = informeEnsayoLiquido.contaminacionParticulas,
+        indiceCetanoFme = informeEnsayoLiquido.indiceCetanoFme,
+        color = informeEnsayoLiquido.color,
+        resultadoFinal = informeEnsayoLiquido.resultadoFinal,
+        remanenteRetirado = informeEnsayoLiquido.remanenteRetirado
+        });
 
             var responseRegistroInformeEnsayoLiquido = responseClienteOsinergmin.registrarInformeLabLiquidoCalidadResponse1;
 
@@ -181,7 +239,28 @@ namespace Infraestructura.Data.MainModule
             var responseClienteOsinergmin = await _osinergminClient.registrarInformeLabGlpCalidadAsync(
                 new informeGlp
                 {
-
+        observaciones = informeEnsayoGlp.Observaciones,
+        numeroInformeLaboratorio = informeEnsayoGlp.NumeroInformeLaboratorio,
+        densidadRelativa = informeEnsayoGlp.DensidadRelativa,
+        presionVapor = informeEnsayoGlp.PresionVapor,
+        numeroOctanoMotor = informeEnsayoGlp.NumeroOctanoMotor,
+        metanoMol = informeEnsayoGlp.MetanoMol,
+        etanoMol = informeEnsayoGlp.EtanoMol,
+        etilenoMol = informeEnsayoGlp.EtilenoMol,
+        propanoMol = informeEnsayoGlp.PropanoMol,
+        propilenoMol = informeEnsayoGlp.PropilenoMol,
+        isobutanoMol = informeEnsayoGlp.IsobutanoMol,
+        nbutanoMol = informeEnsayoGlp.NbutanoMol,
+        trans2butenoMol = informeEnsayoGlp.Trans2butenoMol,
+        butenoMol = informeEnsayoGlp.ButenoMol,
+        isobutilenoMol = informeEnsayoGlp.IsobutilenoMol,
+        cis2butenoMol = informeEnsayoGlp.Cis2butenoMol,
+        isopentanoMol = informeEnsayoGlp.IsopentanoMol,
+        npentanoMol = informeEnsayoGlp.NpentanoMol,
+        butadienoMol = informeEnsayoGlp.ButadienoMol,
+        hexano = informeEnsayoGlp.Hexano,
+        corrosionLaminaCobre = informeEnsayoGlp.CorrosionLaminaCobre,
+        determinacionEtilMercaptano = informeEnsayoGlp.DeterminacionEtilMercaptano
                 });
 
             var responseRegistroInformeEnsayoGlp = responseClienteOsinergmin.registrarInformeLabGlpCalidadResponse1;
